c1739cde449909fdc77973d201683be37659c6c2
DevRoss <820584458@qq.com>
2017-12-19 20:08:45 +0800

    ScoreTable增加getSize和区间人数和百分比的统计

--- src/com/ross/ScoreTable.java
+++ src/com/ross/ScoreTable.java
@@ -2,12 +2,13 @@ package com.ross;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.function.DoubleToIntFunction;
 
 
 /**
  * 成绩单对象，里面存储某门课的学生成绩
  */
-public class ScoreTable implements Serializable{
+public class ScoreTable implements Serializable {
     private ArrayList<TableItem> items;
 
 
@@ -21,5 +22,26 @@ public class ScoreTable implements Serializable{
     public ArrayList<TableItem> getItems() {
         return items;
     }
+
+    public Integer getSize() {
+        return items.size();
+    }
+
+    // 返回在区间 [min, max) 的人数
+    public Integer range(Double min, Double max) {
+        Integer count = 0;
+        for (TableItem item : items) {
+            if (min <= item.score && item.score < max) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // 返回在区间 [min, max) 的人数百分比
+    public Double rangePercentage(Double min, Double max) {
+        return range(min, max) / getSize().doubleValue();
+    }
+
 }
 
