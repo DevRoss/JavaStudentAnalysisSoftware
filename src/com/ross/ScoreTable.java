7ba8d4929d0af815a3fe09654974e9056f071889
DevRoss <820584458@qq.com>
2017-12-19 10:46:29 +0800

    删除ScoreTable中多余的成员score， 将TableItem改为public

--- src/com/ross/ScoreTable.java
+++ src/com/ross/ScoreTable.java
@@ -3,12 +3,13 @@ package com.ross;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+
 /**
  * 成绩单对象，里面存储某门课的学生成绩
  */
 public class ScoreTable implements Serializable{
     private ArrayList<TableItem> items;
-    private Double score;
+
 
     public ScoreTable(ArrayList<Student> students, String course) {
         this.items = new ArrayList<TableItem>();
@@ -16,16 +17,9 @@ public class ScoreTable implements Serializable{
             this.items.add(new TableItem(student.getName(), student.getNum(), student.getCourseScore(course)));
         }
     }
-}
 
-class TableItem implements Serializable{
-    private String name;
-    private String num;
-    private Double score;
-
-    TableItem(String name, String num, Double score) {
-        this.name = name;
-        this.num = num;
-        this.score = score;
+    public ArrayList<TableItem> getItems() {
+        return items;
     }
 }
+
